86951976b0ab826cd45b69044271b7c0125ad6ba
risonakamo <hmykanae@gmail.com>
2018-07-30 15:33:34 -0400

    current selected class render

--- azlelist-system/equipclassmenu.js
+++ azlelist-system/equipclassmenu.js
@@ -2,27 +2,48 @@
 //equipClassNames: array of equip classes
 //classNamesJp: jp string of equip class names
 class EquipClassMenu extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      currentClass: 0 //the index of the current class
+    };
+  }
+
   render() {
     return [React.createElement(
       "div",
       { className: "class-holder", key: "eqmtop1" },
       this.props.equipClassNames.map((x, i) => {
-        return React.createElement(
-          "div",
-          { className: `class-button ${x}`, key: i },
-          React.createElement("img", { src: `icons/${x}.png` }),
-          React.createElement(
-            "div",
-            null,
-            this.props.classNamesJp[i]
-          )
-        );
+        if (i == this.state.currentClass) {
+          return null;
+        }
+
+        return React.createElement(EquipButton, { name: x, nameJp: this.props.classNamesJp[i], key: i });
       })
     ), React.createElement(
       "div",
       { className: "current-class", key: "eqmtop2" },
       React.createElement("div", { className: "float-border" }),
+      React.createElement(EquipButton, { name: this.props.equipClassNames[this.state.currentClass],
+        nameJp: this.props.classNamesJp[this.state.currentClass] }),
       React.createElement("div", { className: "side-triangle" })
     )];
+  }
+}
+
+//EquipButton(string name,string nameJp)
+class EquipButton extends React.Component {
+  render() {
+    return React.createElement(
+      "div",
+      { className: `class-button ${this.props.name}` },
+      React.createElement("img", { src: `icons/${this.props.name}.png` }),
+      React.createElement(
+        "div",
+        null,
+        this.props.nameJp
+      )
+    );
   }
 }
--- azlelist-system/equipclassmenu.jsx
+++ azlelist-system/equipclassmenu.jsx
@@ -3,25 +3,51 @@
 //classNamesJp: jp string of equip class names
 class EquipClassMenu extends React.Component
 {
+  constructor(props)
+  {
+    super(props);
+
+    this.state={
+      currentClass:0 //the index of the current class
+    };
+  }
+
   render()
   {
     return [
       <div className="class-holder" key="eqmtop1">
         {this.props.equipClassNames.map((x,i)=>{
-          return (
-            <div className={`class-button ${x}`} key={i}>
-              <img src={`icons/${x}.png`}/>
-              <div>{this.props.classNamesJp[i]}</div>
-            </div>
-          );
+          if (i==this.state.currentClass)
+          {
+            return null;
+          }
+
+          return <EquipButton name={x} nameJp={this.props.classNamesJp[i]} key={i}/>;
         })}
       </div>,
 
       <div className="current-class" key="eqmtop2">
         <div className="float-border"></div>
 
+        <EquipButton name={this.props.equipClassNames[this.state.currentClass]}
+          nameJp={this.props.classNamesJp[this.state.currentClass]}/>
+
         <div className="side-triangle"></div>
       </div>
     ];
+  }
+}
+
+//EquipButton(string name,string nameJp)
+class EquipButton extends React.Component
+{
+  render()
+  {
+    return (
+      <div className={`class-button ${this.props.name}`}>
+        <img src={`icons/${this.props.name}.png`}/>
+        <div>{this.props.nameJp}</div>
+      </div>
+    );
   }
 }
