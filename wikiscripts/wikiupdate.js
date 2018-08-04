98c43a49a96590de86e613e8d42a1a849b1935ca
risonakamo <hmykanae@gmail.com>
2018-08-04 19:24:55 -0400

    trying things

--- wikiscripts/wikiupdate.js
+++ wikiscripts/wikiupdate.js
@@ -1,13 +1,20 @@
 const request=require("request");
+const IconV=require("iconv").Iconv;
 const {JSDOM}=require("jsdom");
 
 const {jpWikiExtract,extractOptions}=require("./wikiextract-jp");
 
 request.get(
     {
-        url:"http://azurlane.wikiru.jp/index.php?%B4%CF%CB%A4%A5%EA%A5%B9%A5%C8%2F%B6%EE%C3%E0%B4%CF%BC%E7%CB%A4"
+        url:"http://azurlane.wikiru.jp/index.php?%B4%CF%CB%A4%A5%EA%A5%B9%A5%C8%2F%B6%EE%C3%E0%B4%CF%BC%E7%CB%A4",
+        encoding:null
     },
     (err,res,body)=>{
-        console.log(body);
+        var iconv=new IconV("EUC-JP","utf-8");
+
+        var document=new JSDOM(iconv.convert(body).toString("utf8")).window.document;
+
+        // console.log(document.querySelectorAll(".ie5 tbody")[1].innerText);
+        // console.log(jpWikiExtract(document.querySelectorAll(".ie5 tbody")[1],extractOptions.torpedo));
     }
 );
